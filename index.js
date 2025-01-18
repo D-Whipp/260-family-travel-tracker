68e00d7c4dc5ee928360f66724c1e69644a8c82b
David Whipple <dwhipp88@gmail.com>
2025-01-17 17:00:50 -0700

    MANUAL UPDATE: Updated manual for post/add route and cleaned up comments

--- index.js
+++ index.js
@@ -20,24 +20,11 @@ app.use(express.static('public'));
 
 let currentUserId = 1;
 
-// let users = [
-//   { id: 1, name: 'Angela', color: 'teal' },
-//   { id: 2, name: 'Jack', color: 'powderblue' },
-//   { id: 3, name: 'Miles', color: 'darkred' },
-// ];
-
 let users = [];
 let currentUser;
 
-// async function sayGlobalUser() {
-//   console.log('GLOBAL CURRENT USER: ', currentUser);
-// }
-
-// sayGlobalUser();
-
 async function getUsers() {
   const result = await db.query('SELECT * FROM users');
-  // console.log('RESULT: ', result.rows);
   users = [];
   result.rows.forEach((user) => {
     users.push(user);
@@ -53,12 +40,10 @@ async function checkVisisted() {
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  // console.log('COUNTRIES: ', countries);
   return countries;
 }
 app.get('/', async (req, res) => {
   const users = await getUsers();
-  // console.log('USERS', users);
   const countries = await checkVisisted();
   res.render('index.ejs', {
     countries: 0,
@@ -68,50 +53,14 @@ app.get('/', async (req, res) => {
   });
 });
 
-// *******************  2.A  *********************
-// DEPRECATED: Instructors code, Required
-// updating as features progressed.
-// ******************  START  *******************
-
-// *****  SEE 2.B FOR ROUTE UPDATE  *******
-
-// app.post('/add', async (req, res) => {
-//   const input = req.body['country'];
-
-//   try {
-//     const result = await db.query(
-//       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
-//       [input.toLowerCase()]
-//     );
-
-//     const data = result.rows[0];
-//     // console.log('ADD ROUTE DATA: ', data);
-//     const countryCode = data.country_code;
-//     try {
-//       await db.query(
-//         'INSERT INTO visited_countries (country_code) VALUES ($1)',
-//         [countryCode]
-//       );
-//       res.redirect('/');
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-// *******************  2.A  *********************
-// This concludes the above code
-// ******************  END  *********************
-
-// *******************  2.B  *********************
+// *******************  1.B  *********************
 // UPDATING POST/ADD ROUTE
 // ******************  START  *******************
 app.post('/add', async (req, res) => {
   const input = req.body['country'];
 
   try {
-    // *******************  2.B  *********************
+    // *******************  1.B  *********************
     // Same as the original /add route, we take
     // the user's input and check for it in our db
     // ***************  CONTINUED  ****************
@@ -120,11 +69,10 @@ app.post('/add', async (req, res) => {
       "SELECT id FROM countries WHERE LOWER (country_name) LIKE '%' || $1 || '%';",
       [input.toLowerCase()]
     );
-
     const data = result.rows[0];
     const countryID = data.id;
     try {
-      // *******************  2.B  *********************
+      // *******************  1.B  *********************
       // Here we've changed our table to store the
       // user's visited country inside of users_journeys
       // and NOT visited_countries.
@@ -142,38 +90,29 @@ app.post('/add', async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-  // *******************  2.B  *********************
+  // *******************  1.B  *********************
   // This conlcudes the above code
   // *******************  END  ********************
 });
 
 app.post('/user', async (req, res) => {
   const users = await getUsers();
-  // const currentUser = req.body.user;
-
-
   // *******************  1.A - b  *******************
   // ****************** START ********************
   // Using if statement here to render new.ejs so a family
   // member can be added if the Add Family Member
   // option is selected. 
-
   if (req.body.add) {
     res.render('new.ejs');
   }
   // *****************  1.A - b  *******************
   // ******************  END  ********************
-
-
   currentUser = req.body.user;
-  // console.log('CURRENT USER: ', currentUser);
-
   // *******************  1.A  *********************
   // The following code takes the ID's for the
   // countries the current user visited and
   // coverts them into country codes.
   // ******************  START  *******************
-
   const currentUsersCountriesIDs = await db.query(
     'SELECT countries_id FROM users_journeys WHERE user_id=$1',
     [currentUser]
@@ -182,7 +121,6 @@ app.post('/user', async (req, res) => {
   currentUsersCountriesIDs.rows.forEach((country) => {
     usersCountries.push(country.countries_id);
   });
-
   // *******************  1.A  *********************
   // Afterwards, we grab all the country data
   // from our database and check their country
@@ -207,7 +145,6 @@ app.post('/user', async (req, res) => {
   // page rendering.
   // ***************  CONTINUED  ****************
 
-  // sayGlobalUser();
 
   res.render('index.ejs', {
     countries: countries,
@@ -225,17 +162,12 @@ app.post('/new', async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
   //https://www.postgresql.org/docs/current/dml-returning.html
 
-  // res.render('new.ejs', {
-  //   countries: countries,
-  //   total: countries.length,
-  //   users: users,
-  //   color: 'teal',
-  // })
-  // res.render('new.ejs');
-  console.log('/new req: ', req.body);
-  console.log('/new is listening');
-  console.log('Name: ', req.body.name);
-  console.log('Color: ', req.body.color);
+// *******************  1.C  **********************
+// Adding a new user is simple. 
+// Grab the name and color inputs from the req.body
+// and add them to the db using 1 query.
+// Afterwards, redirect to the root.
+// *****************  START  ********************
 
   try {
     
@@ -247,14 +179,9 @@ app.post('/new', async (req, res) => {
     console.log(error)
   }
 
-  // res.render('index.ejs', {
-  //   countries: [],
-  //   total: 0,
-  //   users: users,
-  //   color: 'teal',
-  // });
+    // *******************  1.C  *********************
+   // ******************  END  *********************
 
-  // res.send('new.ejs')
 });
 
 app.listen(port, () => {
